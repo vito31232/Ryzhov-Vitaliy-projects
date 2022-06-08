8596c5490493de034e2eda959ccf1a0bf25a4998
Виталий Рыжов Романович <zergvito31232zerg@mail.ru>
2022-06-08 12:46:12 +0300

    1 project

---
+++ Такси/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.button10 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button12 = new System.Windows.Forms.Button();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button12);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.button11);
+            this.tabPage1.Controls.Add(this.dateTimePicker3);
+            this.tabPage1.Controls.Add(this.button10);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.dateTimePicker2);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button9);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1972, 1054);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "в";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(152, 497);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(32, 20);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(190, 495);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(105, 23);
+            this.button11.TabIndex = 17;
+            this.button11.Text = "прибыль за день";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(6, 497);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(140, 20);
+            this.dateTimePicker3.TabIndex = 16;
+            this.dateTimePicker3.ValueChanged += new System.EventHandler(this.dateTimePicker3_ValueChanged);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(113, 552);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(155, 23);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "сумма заказов за период";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 525);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(32, 20);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(44, 523);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(138, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "заказы с клиентом (ID)";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 551);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "заказы с и по";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(6, 606);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 11;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 580);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 661);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 632);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Поиск";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 152);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Удаление";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(6, 123);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(116, 23);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "Добавить";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(321, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(950, 695);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.button1.Location = new System.Drawing.Point(6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Список клиентов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Заказы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 93);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(116, 24);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Список водителей";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Список операторов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1980, 1080);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(6, 468);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(289, 23);
+            this.button12.TabIndex = 20;
+            this.button12.Text = "Прибыль за всё время";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1274, 715);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Такси";
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Такси/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;   
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string connection = "Server=localhost;Database=TAXI;Uid=root;password=192817istra;charset=utf8";
+        private MySqlConnection conn; //коннектор к БД
+        private MySqlCommand cmd; //
+        private MySqlDataAdapter adapter; //Предоставляет выборку из запроса
+        private System.Data.DataTable dt; //Отображает, что находится в памяти сейчас
+        private BindingSource binding_source;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection(connection);
+            conn.Open();
+        }
+        ~Form1()
+        {
+            conn.Close();
+        }
+        private void send(string dudik)
+        {
+            adapter = new MySqlDataAdapter(dudik, conn);
+            dt = new DataTable();
+            adapter.Fill(dt);
+            binding_source = new BindingSource();
+            binding_source.DataSource = dt;
+            dataGridView1.DataSource = binding_source;
+        }
+        private void send1(string nahui)
+        {
+            adapter = new MySqlDataAdapter(nahui, connection);
+            MySqlCommandBuilder myCommandBuilder = new MySqlCommandBuilder(adapter as MySqlDataAdapter);
+            adapter.InsertCommand = myCommandBuilder.GetInsertCommand();
+            adapter.UpdateCommand = myCommandBuilder.GetUpdateCommand();
+            adapter.DeleteCommand = myCommandBuilder.GetDeleteCommand();
+            dt = new DataTable();   
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt.DefaultView;
+        }
+        private void button1_Click_1(object sender, EventArgs e) // вывод списка клиентов
+        {
+            try
+            {
+                send1("SELECT * FROM taxi.client;");
+            }
+            catch (Exception lol)
+            {
+                MessageBox.Show(lol.Message.ToString());
+            }
+        }
+        private void button2_Click(object sender, EventArgs e) // вывод списка операторов
+        {
+            try
+            {
+                send1("SELECT * FROM taxi.operator;");
+            }
+            catch (Exception lol)
+            {
+                MessageBox.Show(lol.Message.ToString());
+            }
+        }
+        private void button3_Click(object sender, EventArgs e) // вывод списка заказов
+        {
+            try
+            {
+                send1("SELECT * FROM taxi.orders;");
+            }
+            catch (Exception lol)
+            {
+                MessageBox.Show(lol.Message.ToString());
+            }
+        }
+        private void button4_Click(object sender, EventArgs e) // вывод списка водителей
+        {
+            try
+            {
+                send1("SELECT * FROM taxi.driver;");
+            }
+            catch (Exception lol)
+            {
+                MessageBox.Show(lol.Message.ToString());
+            }
+        }
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button9_Click(object sender, EventArgs e) // добавление
+        {
+            try
+            {
+                adapter.Update(dt);
+            }
+            catch (Exception lol)
+            {
+                MessageBox.Show(lol.Message.ToString());
+            }
+        }
+        private void button5_Click(object sender, EventArgs e) // удаление
+        {
+            if(MessageBox.Show("Удалить?","Удаление",MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                try
+                {
+                    dataGridView1.Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+                    adapter.Update(dt);
+                }
+                catch (Exception lol)
+                {
+                    MessageBox.Show(lol.Message.ToString());
+                }
+            }
+        }
+        private void button6_Click(object sender, EventArgs e) // пользовательский поиск
+        {
+            try
+            {
+                send1(textBox1.Text);
+            }
+            catch (Exception lol)
+            {
+                MessageBox.Show(lol.Message.ToString());
+            }
+        }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e) // с
+        {
+
+        }
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e) // по
+        {
+
+        }
+        private void button7_Click(object sender, EventArgs e) // вывод заказов за опр промежуток
+        {
+            try
+            {
+                send1($"select * from taxi.orders where data between '{dateTimePicker1.Value.ToString("yyyy-MM-dd")}' and '{dateTimePicker2.Value.ToString("yyyy-MM-dd")}'");
+            }
+            catch
+            {
+
+            }
+        }
+        private void button8_Click(object sender, EventArgs e) // вывод заказов с опр клиентом ID
+        {
+            try
+            {
+                send1($"select * from taxi.orders where client_ID_client='{textBox2.Text}';");
+            }
+            catch
+            {
+
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e) // поле для ввода ID
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e) // вывод заказов за опр время
+        {
+            try
+            {
+                send($"SELECT SUM(amount) FROM taxi.orders where data between '2022-03-01' and '2022-03-25';"); // забыл сделать datatimepicker но и пофиг
+            }
+            catch 
+            {
+               
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e) // вывод заработной платы у водителя
+        {
+            try
+            {
+                send($"select sum(amount)*0.3 as profit from taxi.orders where driver_ID_driver='{textBox3.Text}' and data between '{dateTimePicker3.Value.ToString("yyyy-MM-dd")}' and '{dateTimePicker3.Value.ToString("yyyy-MM-dd")}';");
+            }
+            catch 
+            {
+
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) // ID водителя для прибыли за день
+        {
+
+        }
+
+        private void dateTimePicker3_ValueChanged(object sender, EventArgs e) // дата для прибыли за день
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e) // вывод прибыли 
+        {
+            try
+            {
+                send($"select sum(amount)*0.7 as profit from taxi.orders;");
+            }
+            catch
+            {
+
+            }
+        }
+    }
+    }
+  
